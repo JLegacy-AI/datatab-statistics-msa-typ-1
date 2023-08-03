5bd21984674c7dfa0cd259d7e6d0bf19f29e12df
jlegacy-ai <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2023-08-03 21:24:55 +0500

    Minor Changes

--- src/pages/msa-type-1/index.jsx
+++ src/pages/msa-type-1/index.jsx
@@ -97,7 +97,7 @@ const MsaType1 = () => {
   // Handle Print
   const handleToPrint = useReactToPrint({
     content: () => printComponentRef.current,
-    documentTitle: "Datatab Result & Statistics",
+    documentTitle: "Datatab Report",
     pageStyle: `@media print {
         @page {
           size: 300mm 300mm;
@@ -184,7 +184,7 @@ const MsaType1 = () => {
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 onClick={() => handleToPrint()}
               >
-                Print Result & Statistics
+                Print Report
               </button>
             </div>
           </>
