eea6462ca708a3734eefda119175fd2ee744b289
jlegacy-ai <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2023-09-05 01:02:26 +0500

    Minor Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import StickyHeader from "./components/StickyHeader";
 import MsaType1 from "./pages/msa-type-1";
 
+import { lazy, Suspense } from "react";
+
+const StickyHeader = lazy(() => import("./components/StickyHeader"));
+
 function App() {
   return (
     <div className="App font-poppins">
       <StickyHeader />
       <div className="px-20 py-4  mt-[130px]">
-        <MsaType1 />
+        <Suspense
+          fallback={
+            <>
+              <p className="w-screen h-screen">Loading...</p>
+            </>
+          }
+        >
+          <MsaType1 />
+        </Suspense>
       </div>
     </div>
   );
